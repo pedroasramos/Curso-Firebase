8184727cf88bc874cdfbf6b39219ecc22c44d20a
Pedro Ramos <pedroasramos.ti@gmail.com>
2021-09-23 17:49:24 -0300

    removendo dados do database

--- TODOLIST/js/todo.js
+++ TODOLIST/js/todo.js
@@ -25,7 +25,24 @@ function fillTodoList(dataSnapshot){
         var li = document.createElement('li') // Cria um elemento do tipo li
         var spanLi = document.createElement('span') // Cria um elemento do tipo span
         spanLi.appendChild(document.createTextNode(value.name)) // Adiciona um elemento de texto dentro da span
+        spanLi.id = item.key // Define o id do spanLi com a chave da tarefa
         li.appendChild(spanLi) // Adiciona o span dentro do li
+        var liRemoveBtn = document.createElement('button') // Cria um botão para remoção da tarefa
+        liRemoveBtn.appendChild(document.createTextNode('Excluir')) //Define o texto do botão
+        liRemoveBtn.setAttribute('onclick', 'removeTodo(\"' + item.key +'\")') // Configura o onclick do botão de remoção de tarefas
+        liRemoveBtn.setAttribute('class', 'danger todoBtn') // Define classes de estilização para o botão de remoção
+        li.appendChild(liRemoveBtn) // Adiciona o botão de remoção no li
         ulTodoList.appendChild(li) // Adiciona o li dentro da lista de tarefas
     })
+}
+
+// Remove tarefas
+function removeTodo(key) {
+    var selectedItem = document.getElementById(key)
+    var confirmation = confirm('Realmente deseja remover a tarefa \"' + selectedItem.innerHTML +'\"?')
+    if(confirmation) {
+        dbRefUsers.child(firebase.auth().currentUser.uid).child(key).remove().catch(function(error){
+            showError('Falha ao remover tarefa: ', error)
+        })
+    }
 }
